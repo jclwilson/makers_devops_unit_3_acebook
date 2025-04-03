3e4449c323f1fd64e7236671dede9dbbc7283255
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-04-03 10:10:31 +0100

    Update application_start.sh

--- application_start.sh
+++ application_start.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# Add NVM to the command list
+source /home/ec2-user/.nvm/nvm.sh
 
+# Move to Acebook directory
 cd /home/ec2-user/acebook
 
+# Install PM2
 npm install -g pm2@latest
 
 # Run PM2 and point it to config file
